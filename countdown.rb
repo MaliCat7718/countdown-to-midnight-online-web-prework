49ecfb77b5eca9b3644a148ce3a7c610a2f1ed98
Malary Robinson <robinmal16@gmail.com>
2019-04-20 20:52:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,13 @@ def countdown(number)
 end
 
 countdown(10)
+
+def countdown_with_sleep(number)
+  while number > 0
+    sleep 1
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+    return "HAPPY NEW YEAR!"
+  end
+end
